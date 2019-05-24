aa1e165d01ac4fa9aceb99d537459e3ac6bc508f
Rick Harris <rickharris@users.noreply.github.com>
2019-05-24 11:06:27 -0700

    revert some adjustments made to reduce firmware size

--- keyboards/niu_mini/keymaps/planck/config.h
+++ keyboards/niu_mini/keymaps/planck/config.h
@@ -30,23 +30,3 @@
 //#define MIDI_TONE_KEYCODE_OCTAVES 2
 
 #endif
-
-
-#if !defined(NO_DEBUG) && !defined(CONSOLE_ENABLE)
-
-#define NO_DEBUG
-
-#endif // !NO_DEBUG
-
-#if !defined(NO_PRINT) && !defined(CONSOLE_ENABLE)
-
-#define NO_PRINT
-
-#endif // !NO_PRINT
-
-#define NO_ACTION_MACRO
-
-#define NO_ACTION_FUNCTION
-
-#define DISABLE_LEADER
-#define MIDI_BASIC
